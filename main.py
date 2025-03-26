7d586cd8ef924c62bba47cc9433dffad7311ee75
v-roman <r.v.v.05112001@gmail.com>
2025-03-26 09:54:41 +0300

    main.py

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
-password = input('Введите пароль: ')
-
 def is_very_long(password):
-    if len(password) > 12:
-        return True
-    else:
-        return False
+    return len(password) > 12
 
 def has_digit(password):
-    for number in password:
-        return any(number.isdigit() for number in password)
+    return any(number.isdigit() for number in password)
 
 def has_letters(password):
-    for letters in password:
-        return any(letters.isalpha() for letters in password)
+    return any(letters.isalpha() for letters in password)
 
 def has_upper_letters(password):
-    for upper_letters in password:
-        return any(upper_letters.isupper() for upper_letters in password)
+    return any(upper_letters.isupper() for upper_letters in password)
 
 def has_lower_letters(password):
-    for lower_letters in password:
-        return any(lower_letters.islower() for lower_letters in password)
+    return any(lower_letters.islower() for lower_letters in password)
 
 def has_symbols(password):
-    for symbols in password:
-        return any(symbols.isalpha() == False and symbols.isdigit() == False for symbols in password)
+    return any(symbols.isalpha() == False and symbols.isdigit() == False for symbols in password)
 
-function = [
-    has_digit(password),
-    is_very_long(password),
-    has_letters(password),
-    has_upper_letters(password),
-    has_symbols(password),
-]
+def main():
+    password = input('Введите пароль: ')
 
-score = 0
-for i in function:
-    if i == True:
-        score = score + 2
-    else:
-        score = score
+    function = [
+        has_digit(password),
+        is_very_long(password),
+        has_letters(password),
+        has_upper_letters(password),
+        has_symbols(password),
+    ]
 
-print('Рейтинг пароля:', score)
+    score = 0
+    for character in function:
+        if character == True:
+            score = score + 2
+        else:
+            score = score
+    print('Рейтинг пароля:', score)
+
+if __name__ == "__main__":
+    main()
